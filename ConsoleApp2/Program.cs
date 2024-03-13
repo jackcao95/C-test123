b23fffe1dbb1ccab13e353d24fc3dc5b3f312373
jackcao <690724830@qq.com>
2024-03-13 14:08:22 +0800

    C#init

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point a = new Point(23, 15);
+            a.ConPoint(a);
+
+            Console.Read();
+        }
+
+        public class Point
+        {
+            public int x;
+            public int y;
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public void ConPoint(dynamic point)
+            {
+                Console.WriteLine($"坐标点的位置为x:{point.x},y:{point.y}");
+            }
+        }
+
+        
+    }
+}
